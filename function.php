97dfb14ca7e6c42a85ded14904c315f6d740e166
bro shaQ <tkm.kwsm@gmail.com>
2019-10-15 17:53:31 +0900

    herokuへデプロイ

--- function.php
+++ function.php
@@ -182,9 +182,9 @@ function isLogin(){
   }
 }
 
-//データベース
+//データベース(local用)
 //DB接続関数
-function dbConnect(){
+/* function dbConnect(){
     $dsn = 'mysql:dbname=foryou;host=localhost;charset=utf8';
     $user = 'root';
     $password = 'root';
@@ -195,6 +195,21 @@ function dbConnect(){
     );
     $dbh = new PDO($dsn, $user, $password, $options);
     return $dbh;
+} */
+//本番用データベース
+function dbConnect(){
+  $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
+  $db['dbname'] = ltrim($db['path'], '/');
+  $dsn = "mysql:host={$db['host']};dbname={$db['dbname']};charset=utf8";
+  $user = $db['user'];
+  $password = $db['pass'];
+  $options = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
+  );
+  $dbh = new PDO($dsn,$user,$password,$options);
+  return $dbh;
 }
 //SQL実行関数
 //function queryPost($dbh, $sql, $data){
